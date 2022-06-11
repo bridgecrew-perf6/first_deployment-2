03d61d8f83de4a863c6c09bd536caa76036728aa
samantha <langridge.se@gmail.com>
2022-06-11 10:46:39 -0600

    added app launch

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from dbcreds import *
+from helpers.dbhelpers import run_query
 
 app = Flask(__name__)
 
 import sys
+
+
+
+@app.post('/api/animals')
+def animals_post():
+    data = request.json
+    animal_name=data.get('animalName')
+    image_url=data.get('imageUrl')
+    # if animal name == None - this is equivalent to below
+    if not animal_name:
+        return jsonify("Missing required argument 'animalName'"), 422
+    if not image_url:
+        return jsonify("Missing required argument 'imageUrl'"), 422
+    # TODO: Error checking the actual values for the arguments
+    #TODO: DB write
+    return jsonify("Animal added"), 201
+
+
+
+@app.get('/api/animals')
+def animals_get():
+    #todo: db select
+    animal_list=[]
+    return jsonify(animal_list), 200
+
 
 if (len(sys.argv)>1):
     mode = sys.argv[1]
@@ -24,5 +50,3 @@ else:
     exit() 
 
 app.run(debug=True)
-
-app.get
